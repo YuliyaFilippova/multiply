8779392c00e3c22ee66cd4ca672a0b532c0e1837
YuliyaFilippova <yuliya.kazimirchanka@gmail.com>
2019-10-01 15:43:02 +0300

    feat: add multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(a, b) {
+  a = BigInt(a);
+  b = BigInt(b);
+  return  ''+ (a*b); 
+
+  /* var aa = a.split('').reverse();
+  var bb = b.split('').reverse();
+
+  var result = [];
+
+  for (var i = 0; i < aa.length; i++) {
+    for (var j = 0; j < bb.length; j++) {
+      var m = aa[i] * bb[j];
+      result[i + j] = (result[i + j]) ? result[i + j] + m : m;
+    }
+  }
+
+  for (var i = 0; i < result.length; i++) {
+    var num = result[i] % 10;
+    var move = Math.floor(result[i] / 10);
+    result[i] = num;
+
+    if (result[i + 1])
+      result[i + 1] += move;
+    else if (move != 0)
+      result[i + 1] = move;
+  }
+
+  return result.reverse().join('');*/ 
+
 }
